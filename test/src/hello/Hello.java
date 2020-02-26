5a64da1504edbbd965afe8c092d6db4824a5135c
Yuan <948919534@qq.com>
2020-02-26 19:00:34 +0800

    for test

---
+++ test/src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world!");
+	}
+
+}
